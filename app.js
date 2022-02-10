ad82d986313f6b8f773f380a07c2f6481745d7f9
Ivan Petrenko <ivanpetrenko.exceedteam@gmail.com>
2022-02-10 12:15:36 +0300

    remove body-parser

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
@@ -11,7 +10,6 @@ app.use(cors());
 const uri = "mongodb+srv://user:user@cluster0.x2u0b.mongodb.net/todoList?retryWrites=true&w=majority";
 mongoose.connect(uri, { useUnifiedTopology: true });
 
-app.use(bodyParser.json());
 app.use('/', apiRoutes);
 
 app.listen(8000, () => {});
