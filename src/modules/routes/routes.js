646d3eef474bbf3c8240836894d701321023b79d
Ivan Petrenko <ivanpetrenko.exceedteam@gmail.com>
2022-02-14 14:27:14 +0300

    last version

--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    getAllTasks,
-    createNewTask,
-    changeTaskInfo,
-    deleteTask
+  getAllTasks,
+  createNewTask,
+  changeTaskInfo,
+  deleteTask
 } = require('../controllers/task.controller');
 
 router.get('/allTasks', getAllTasks);
